bd8671ef6dba3496de45063d04c4a0b29eeaf992
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-02-26 17:13:43 +0900

    update

--- logreg.py
+++ logreg.py
@@ -25,17 +25,93 @@ The result suggest 'when feature[2] = 1, t will be 0' etc.
 
 
 >>> for p, t in zip(phi, vec_t):
-...     y = sigmoid(p.dot(weight))
-...     print(y, y > 0.5, t, ((y > 0.5) == (t == 1)))
-0.589573447422 True 1 True
-0.310135768302 False 0 True
-0.723316565544 True 1 True
-0.589573447422 True 0 False
-0.310135768302 False 0 True
-0.723316565544 True 1 True
-0.589573447422 True 1 True
-0.310135768302 False 0 True
-0.723316565544 True 1 True
+...     print_result(p, weight, t)
+p=0.59 +:+ OK
+p=0.31 -:- OK
+p=0.72 +:+ OK
+p=0.59 +:- NG
+p=0.31 -:- OK
+p=0.72 +:+ OK
+p=0.59 +:+ OK
+p=0.31 -:- OK
+p=0.72 +:+ OK
+
+>>> phi = array([
+...     [1, 1],
+...     [1, 0],
+...     [0, 1],
+...     [0, 0],
+...     ])
+
+>>> vec_t = array(
+...     [1, 0, 0, 1])
+
+>>> weight = learn((phi, vec_t), 1000)
+>>> for p, t in zip(phi, vec_t):
+...     print_result(p, weight, t)
+p=0.50 0:+ NG
+p=0.50 0:- NG
+p=0.50 0:- NG
+p=0.50 0:+ NG
+
+>>> phi = array([
+...     [1, 1, 1, 0],
+...     [1, 0, 0, 0],
+...     [0, 1, 0, 0],
+...     [0, 0, 0, 1],
+...     ])
+
+>>> vec_t = array(
+...     [1, 0, 0, 1])
+
+>>> weight = learn((phi, vec_t), 1000)
+>>> for p, t in zip(phi, vec_t):
+...     print_result(p, weight, t)
+p=0.58 +:+ OK
+p=0.48 -:- OK
+p=0.48 -:- OK
+p=0.60 +:+ OK
+
+>>> phi = array([
+...     [ 1, 1, 0, 0],
+...     [ 1,-1, 1, 1],
+...     [ 1,-1, 1,-1],
+...     [-1, 0, 1, 1],
+...     [-1, 0, 1,-1],
+...     [-1, 0,-1, 0],
+...     ])
+
+>>> vec_t = array([
+...     [ 1, 0, 0, 0, 0, 0],
+...     [ 0, 1, 0, 1, 0, 0],
+...     [ 0, 0, 1, 0, 1, 1]])
+
+>>> weights = [learn((phi, vec_t[i]), 1000) for i in range(3)]
+>>> for i in range(3):
+...     print(i)
+...     for p, t in zip(phi, vec_t[i]):
+...         print_result(p, weights[i], t)
+0
+p=0.80 +:+ OK
+p=0.31 -:- OK
+p=0.31 -:- OK
+p=0.24 -:- OK
+p=0.24 -:- OK
+p=0.50 +:- NG
+1
+p=0.43 -:- OK
+p=0.82 +:+ OK
+p=0.35 -:- OK
+p=0.79 +:+ OK
+p=0.31 -:- OK
+p=0.46 -:- OK
+2
+p=0.30 -:- OK
+p=0.21 -:- OK
+p=0.68 +:+ OK
+p=0.29 -:- OK
+p=0.77 +:+ OK
+p=0.67 +:+ OK
 """
 
 from pylab import *
@@ -44,32 +120,9 @@ from pylab import *
 # without regularization, it may fail to invert mat_H.
 WITH_REGULARIZATION = True
 
-def data_from_file():
-    phi = []  # feature matrix
-    mat_t = []
-    for line in file('3value.csv'):
-        x, t = line.split('\t')
-        phi.append([1] + [float(v) / 2 - 0.5 for v in x.split(',')[:-1]])
-        mat_t.append(map(int, t.split(',')))
-    phi = array(phi)
-    mat_t = array(mat_t)
-    NUM_DATA, NUM_FEATURES = phi.shape
-    NUM_DATA2, NUM_CLASS = mat_t.shape
-    assert NUM_DATA2 == NUM_DATA
-
-    if not 'find largest teacher class':
-        for i in range(NUM_CLASS):
-            print i,
-            vec_t = mat_t[:,i]
-            print vec_t[vec_t != 0].shape
-
-    vec_t = mat_t[:,14]
-    phi = phi[vec_t != 0]
-    vec_t = vec_t[vec_t != 0] / 2 + 0.5
-    return phi, vec_t
-
 def sigmoid(x):
     return 1 / (1 + exp(-x))
+
 
 def learn(data=None, num_iter=100):
     if not data:
@@ -97,9 +150,34 @@ def learn(data=None, num_iter=100):
     return weight
 
 
+def get_prob(vec_x, weight):
+    return sigmoid(array(vec_x).dot(weight))
+
+
+def get_multi_prob(vec_x, weights):
+    probs = [exp(vec_x.dot(w)) for w in weights]
+    sum_probs = sum(probs)
+    return [p / sum_probs for p in probs]
+
+
+def sort_weight(weight):
+    return list(sorted(enumerate(weight), key=lambda x:-abs(x[1])))
+
+
+def print_result(vec_x, weight, t):
+    y = get_prob(vec_x, weight)
+    print "p=%.2f %s:%s %s" % (
+        y,
+        '0+-'[cmp(y, 0.5)],
+        '-+'[t],
+        ['NG', 'OK'][cmp(y, 0.5) == cmp(t, 0.5)]
+        )
+
+
 def _test():
     import doctest
     doctest.testmod()
+
 
 if __name__ == '__main__':
     _test()
